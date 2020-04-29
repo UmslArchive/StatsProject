bb6f7b3582448cd9408998b76512892cdab092b8
Colby <colbyackerman@gmail.com>
2020-04-29 00:56:02 -0500

    Move convertResponse into Utility class

--- src/main/java/Core/Analytics.java
+++ src/main/java/Core/Analytics.java
@@ -1,40 +1,12 @@
 package Core;
 
-import static Models.MemeQuestionnaire.*;
-import static Models.Demographics.*;
-
 public class Analytics {
 
-    private String convertResponse(final String response) {
-        switch (response) {
-            case NO:
-            case STRONGLY_DISAGREE:
-            case NOT_AT_ALL:
-            case SOMETIMES:
-            case RARELY:
-                return "0";
+    public static void cleanupData(Table table) {
 
-            case YES:
-            case DISAGREE:
-            case A_LOT:
-            case ALL_THE_TIME:
-                return "1";
-
-            case NEUTRAL:
-                return "2";
-
-            case AGREE:
-                return "3";
-
-            case STRONGLY_AGREE:
-                return "4";
-
-            default:
-                return response;
-        }
     }
 
-    public static void exportPivotTableSource(String outFilepath) {
+    public static void exportPivotTableSource(Table table, String outFilepath) {
 
     }
 
@@ -44,5 +16,4 @@ public class Analytics {
  * TODO:
  *  * Re-encode how-often-social-media response
  *  * Cleanup data inconsistencies in free-response columns (textbox)
- *
  * **/
