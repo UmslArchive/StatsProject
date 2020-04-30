5bb6a60ee58216208b4d73bef52fd45ac53d896c
Colby <colbyackerman@gmail.com>
2020-04-30 00:12:07 -0500

    Add social media frequency score conversion column

--- src/main/java/Models/Demographics.java
+++ src/main/java/Models/Demographics.java
@@ -8,10 +8,10 @@ import java.util.List;
 public class Demographics {
 
     public static final String ALL_THE_TIME = "All The Time";
-    public static final String A_LOT = "A Lot";
+    public static final String A_LOT = "A lot";
     public static final String SOMETIMES = "Sometimes";
     public static final String RARELY = "Rarely";
-    public static final String NOT_AT_ALL = "Not At All";
+    public static final String NOT_AT_ALL = "Not at all";
 
     public List<String> responses = new ArrayList<>(FormInfo.demographicSize);
 }
